b04bac85b6427fadfe1714fdf53aef5bffebf697
Soheil N <soheil.nrz@gmail.com>
2017-03-16 22:03:59 -0700

    Minor fix to settings activity navigation

--- app/src/main/java/com/example/soheiln/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/soheiln/popularmovies/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.soheiln.popularmovies;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by distributionlab on 3/15/17.
@@ -16,5 +18,15 @@ public class SettingsActivity extends AppCompatActivity {
 
         // Enable the Up button in the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // When the home button is pressed, take the user back to the VisualizerActivity
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
